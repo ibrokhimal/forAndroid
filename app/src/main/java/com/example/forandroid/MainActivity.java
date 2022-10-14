4ed8312b3d1c309b75a58d48129cd7f9540431a6
ibrokhimal <102519725+ibrokhimal@users.noreply.github.com>
2022-10-14 14:41:54 +0500

    активитий готов

--- app/src/main/java/com/example/forandroid/MainActivity.java
+++ app/src/main/java/com/example/forandroid/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.forandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
